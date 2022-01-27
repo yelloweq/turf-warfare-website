1c6d44a808b5f8354795f2b4af73e28763264a58
Yelloweq <iwo.grzes237@gmail.com>
2022-01-27 11:25:08 +0000

    fixed nav overflow

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom';
 
 function Header(props) {
     return (
-        <header className="d-flex  align-items-center justify-content-center justify-content-md-between py-3 text-white">
-            <div className='d-flex col-md-3 justify-content-center'>
+        <header className="d-flex align-items-center justify-content-center justify-content-md-between py-3 text-white">
+            <div className='d-flex col-1 justify-content-center'>
                 <a href="/" className="logo">Logo</a>
             </div>
             
 
             {props.children}
 
-            <div className='d-flex col-md-3 justify-content-center'>
+            <div className='me-5'>
                 <Link to="/login" className="btn btn-outline-primary me-5">sign in</Link>
 
                 <Link to="/login" className="btn btn-primary">Play Now</Link>
