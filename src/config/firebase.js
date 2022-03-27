c4a12d0a318155008d58bc1f1a4bbd3ffc713167
Iwo Grzes <iwo.grzes237@gmail.com>
2022-03-27 14:06:43 +0100

    leaderboard prototype

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app"
 import "firebase/compat/firestore"
 import "firebase/compat/auth"
+import "firebase/compat/database"
 
 const app = firebase.initializeApp({
   apiKey: "AIzaSyCkw22mRKzFfNmcTvQBN4C7Qg15LXN1Sug",
--- src/pages/leaderboard/index.js
+++ src/pages/leaderboard/index.js
@@ -1,38 +1,57 @@
-// import { onValue, getDatabase, query, ref, orderByChild, orderBy } from 'firebase/database'
-import React from 'react'
-// import { useAuth } from '../../contexts/AuthContext'
+import firebase from '../../config/firebase'
+import React, { useState, useEffect } from 'react'
+import { useAuth } from '../../contexts/AuthContext'
 import './Leaderboard.scss'
 
 function Leaderboard() {
 
-    // const [loading, setLoading] = useState(true)
-    // const [leaderboard, setLeaderboard] = useState([])
-    // const { currentUser } = useAuth()
+    const [loading, setLoading] = useState(true)
+    const [leaderboard, setLeaderboard] = useState([])
+    const { currentUser } = useAuth()
 
-    // useEffect(() => {
-    //   if (currentUser) {
-    //     const db = getDatabase();
-    //     const mostWins = query(ref(db, 'users/'));
-    //     onValue(mostWins, (snapshot) => {
-    //       console.log(snapshot.val())
-    //       const data = snapshot.val()
-    //       // console.log(data);
-    //     })
-    //   }
-    // }, [])
+    function sortByWins(a , b){
+      if (a.wins < b.wins){
+        return 1;
+      }
+      if (a.wins > b.wins){
+        return -1;
+      }
+      return 0;
+    }
+
+    useEffect(() => {
+      if (currentUser) {
+        const db = firebase.database().ref("users");
+        db.on("value", (snapshot) => {
+          const users = snapshot.val();
+          const ranking = [];
+          for  (let id in users){
+            ranking.push(users[id])
+          }
+          ranking.sort(sortByWins)
+          setLeaderboard(ranking);
+          setLoading(false);
+        })
+      }
+    }, [])
 
     
-    // if (loading) {
-    //     return <div className='container-xxl container-height border text-white mb-2 align-items-center justify-content-center'>
-    //         <h1 className='display 2'>No data</h1>
-    //     </div>
-    // }
+    if (loading) {
+        return <div className='container-xxl container-height border text-white mb-2 align-items-center justify-content-center'>
+            <h1 className='display 2'>No data</h1>
+        </div>
+    }
 
   return (
   <>
     <div className='container-xxl container-height border text-white mb-2'>
-      
-      </div>
+      {leaderboard ? (leaderboard.map((user) => {
+        return (
+          <ul key={user.username}>{user.username} <span> {user.wins} </span> </ul>
+          
+        )
+      })) : null }
+    </div>
     <div className='container-xxl text-white justify-content-center'>
             <nav aria-label="pages">
                 <ul className="pagination justify-content-center">
