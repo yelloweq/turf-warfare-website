09af6ea1e2143c8d7dfe0bfc7f223fe4790d5608
Iwo <iwo.grzes237@gmail.com>
2022-04-05 17:41:02 +0100

    updated download link to ver 1.2

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,67 +1,109 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
-import './Nav.scss';
-import mylogo from '../../resources/img/logo.png'
-import { useAuth } from '../../contexts/AuthContext'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Nav.scss";
+import mylogo from "../../resources/img/logo.png";
+import { useAuth } from "../../contexts/AuthContext";
 
 function Nav() {
-    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
-    const { currentUser, logout } = useAuth();
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const { currentUser, logout } = useAuth();
 
-    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
-            <div className='container-fluid'>
-            <a href="/" className="cannon-logo navbar-brand">
-                <img src={mylogo} className='logo-image' alt="cannon logo "/>
-         </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
-            <span className="navbar-toggler-icon"></span>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+      <div className="container-fluid">
+        <a href="/" className="cannon-logo navbar-brand">
+          <img src={mylogo} className="logo-image" alt="cannon logo " />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded={!isNavCollapsed ? true : false}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
-        
-        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse justify-content-between`} id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-            <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <Link to="/" className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/blog" className="nav-link">blog</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/patch-notes" className="nav-link">Patch Notes</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/how-to-play" className="nav-link">How to play</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/leaderboard" className="nav-link">leaderboard</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/about" className="nav-link">about us</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/contact" className="nav-link">contact</Link>
-                    </li>
-                    </ul>
-            </div>
-        
-        {(currentUser) ?
-            <div className='form-inline expanded-buttons'>
-            <button onClick={logout} className="btn btn-outline-primary ">Log out</button>
-            <a href="https://drive.google.com/file/d/1OLXh1wnbZ1RXCKFJK8F8z6zd-czYukz_/view?usp=sharing" className="btn btn-primary ms-4">Download</a>
-            </div> :   
-                <div className='form-inline expanded-buttons'>
-                <Link to="/login" className="btn btn-primary ms-4">Log in</Link>
-                <a href="https://drive.google.com/file/d/1OLXh1wnbZ1RXCKFJK8F8z6zd-czYukz_/view?usp=sharing" className="btn btn-primary ms-4">Download</a>
-                </div>
-              } 
-               </div>
-            </div>
-        </nav>
 
-    )
+        <div
+          className={`${
+            isNavCollapsed ? "collapse" : ""
+          } navbar-collapse justify-content-between`}
+          id="navbarNavAltMarkup"
+        >
+          <div className="navbar-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/blog" className="nav-link">
+                  blog
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/patch-notes" className="nav-link">
+                  Patch Notes
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/how-to-play" className="nav-link">
+                  How to play
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/leaderboard" className="nav-link">
+                  leaderboard
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-link">
+                  about us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link">
+                  contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {currentUser ? (
+            <div className="form-inline expanded-buttons">
+              <button onClick={logout} className="btn btn-outline-primary ">
+                Log out
+              </button>
+              <a
+                href="https://drive.google.com/file/d/1ngYbu2BpmjTc0DLiPcGepsJqV5sLHUF3/view?usp=sharing"
+                className="btn btn-primary ms-4"
+              >
+                Download
+              </a>
+            </div>
+          ) : (
+            <div className="form-inline expanded-buttons">
+              <Link to="/login" className="btn btn-primary ms-4">
+                Log in
+              </Link>
+              <a
+                href="https://drive.google.com/file/d/1ngYbu2BpmjTc0DLiPcGepsJqV5sLHUF3/view?usp=sharing"
+                className="btn btn-primary ms-4"
+              >
+                Download
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,46 +1,46 @@
-import React from 'react'
-import './Home.scss'
-import logo1 from'../../resources/img/logo.png'
-
-
-
+import React from "react";
+import "./Home.scss";
+import logo1 from "../../resources/img/logo.png";
 
 export default function Home() {
-    return (
-       <div className="landing">
-	   <img id="landing-logo" src={logo1} alt="Logo" className="center"></img>
-        <div className ="text">
-                <h1 className='display-4 fw-bold 1h-1 mb-3'>Trench Warfare</h1>    
-                <h2>
-                Experience a unique battle against formidable opponents<br/> and start your journey to becoming the king of the bloody hills<br/> by using everything to your adavntage!
-                <br />
-                <br />
-                Build out your base and weapons by earning resources during the fight to dominate the battle field.
-                <br />
-                Cannons, Guns, Grenades. Trench Warfare has it all!
-                <br />
-                <br />
-                Get out there and show yourself on the leaderboards today!<br/><br/>
-			Available on Windows Only
-			
-                </h2>
-				<div className="play-wrapper">
-                <a href="https://drive.google.com/file/d/1OLXh1wnbZ1RXCKFJK8F8z6zd-czYukz_/view?usp=sharing" className="play-button">Download</a>
-                </div>
-                </div>
+  return (
+    <div className="landing">
+      <img id="landing-logo" src={logo1} alt="Logo" className="center"></img>
+      <div className="text">
+        <h1 className="display-4 fw-bold 1h-1 mb-3">Trench Warfare</h1>
+        <h2>
+          Experience a unique battle against formidable opponents
+          <br /> and start your journey to becoming the king of the bloody hills
+          <br /> by using everything to your adavntage!
+          <br />
+          <br />
+          Build out your base and weapons by earning resources during the fight
+          to dominate the battle field.
+          <br />
+          Cannons, Guns, Grenades. Trench Warfare has it all!
+          <br />
+          <br />
+          Get out there and show yourself on the leaderboards today!
+          <br />
+          <br />
+          Available on Windows Only
+        </h2>
+        <div className="play-wrapper">
+          <a
+            href="https://drive.google.com/file/d/1ngYbu2BpmjTc0DLiPcGepsJqV5sLHUF3/view?usp=sharing"
+            className="play-button"
+          >
+            Download
+          </a>
+        </div>
+      </div>
 
-                <br/><br/><br/><br/><br/><br/>
-
-
-            </div>
-			
-			
-		
-			
-         
-		
-       
-       
-       
-    )
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
 }
